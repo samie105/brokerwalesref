18e45cdf750ecdcba4e299e0679abf755dae8ab1
samie105 <samsonrichfield@gmail.com>
2024-07-14 07:51:17 +0100

    Auth: fixing server action production issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-export const experimental = {
-  serverActions: {
-    allowedOrigins: ["wilsonbank.vercel.app", "*.vercel.app"],
+module.exports = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["wilsonbank.vercel.app", "*.vercel.app"],
+    },
   },
 };
