faee1c36bca63dd4b33d6f118d4a50609b7f9969
samie105 <samsonrichfield@gmail.com>
2024-07-14 09:28:41 +0100

    ff

--- components/auth/signup/SignUpForm.tsx
+++ components/auth/signup/SignUpForm.tsx
@@ -6,7 +6,6 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useColors } from "@/context/colorContext";
-import { TypeOf, z } from "zod";
 import { useSignUpContext } from "@/context/signUpFormContext";
 import { signUpSchema } from "@/server/schema";
 import { checkEmail } from "@/server/actions/checkEmail";
