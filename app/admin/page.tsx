2a914787c96309579ec82a9617d6e5e065e57a00
samie105 <samsonrichfield@gmail.com>
2024-10-10 22:12:41 +0100

    Admin: fixing fetching issue

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,10 +6,9 @@ export default async function page() {
   const rawData = await User.find();
   const data: IUser | null = JSON.parse(JSON.stringify(rawData));
 
-  return (
+  if (data)
     <div className="w-full h-screen bg-white dark:bg-neutral-950">
       {" "}
       <Admin initialData={data} />
-    </div>
-  );
+    </div>;
 }
