0f203c17a9545b309557c118033520e011a22a9d
samie105 <samsonrichfield@gmail.com>
2024-10-11 10:59:28 +0100

    Admin: added dbconnect()

--- app/admin/edit/[email]/page.tsx
+++ app/admin/edit/[email]/page.tsx
@@ -1,8 +1,11 @@
 import UserEdit from "@/components/admin/UserEdits/DetailsEdit";
+import dbConnect from "@/server";
 import User, { IUser } from "@/server/userSchema";
 import React from "react";
 
 export default async function Edit({ params }: { params: { email: string } }) {
+  await dbConnect();
+
   const email = params.email.replace("%40", "@");
   const rawData = await User.findOne({ email });
   const data: IUser | null = JSON.parse(JSON.stringify(rawData));
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,10 @@
 import Admin from "@/components/admin";
+import dbConnect from "@/server";
 import User, { IUser } from "@/server/userSchema";
 import React from "react";
 
 export default async function page() {
+  await dbConnect();
   const rawData = await User.find({});
   const data: IUser | null = JSON.parse(JSON.stringify(rawData));
 
