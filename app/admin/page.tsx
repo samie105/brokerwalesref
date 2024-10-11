6e0514eb27619e71d743d3354e472ea9178a4293
samie105 <samsonrichfield@gmail.com>
2024-10-11 10:29:38 +0100

    Admin: fixed rendering issue 2

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,13 +3,13 @@ import User, { IUser } from "@/server/userSchema";
 import React from "react";
 
 export default async function page() {
-  const rawData = await User.find();
+  const rawData = await User.find({});
   const data: IUser | null = JSON.parse(JSON.stringify(rawData));
 
   return (
     <div className="w-full h-screen bg-white dark:bg-neutral-950">
       {" "}
-      {data && <Admin initialData={data} />}
+      <Admin initialData={data} />
     </div>
   );
 }
