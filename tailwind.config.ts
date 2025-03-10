ba9ef9452d951c1ad5b9f7d291b662457e94a2b7
samie105 <samsonrichfield@gmail.com>
2025-03-10 13:17:22 +0100

    changed tailwind.config.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -99,22 +99,6 @@ const config = {
             height: "0",
           },
         },
-        "accordion-down": {
-          from: {
-            height: "0",
-          },
-          to: {
-            height: "var(--radix-accordion-content-height)",
-          },
-        },
-        "accordion-up": {
-          from: {
-            height: "var(--radix-accordion-content-height)",
-          },
-          to: {
-            height: "0",
-          },
-        },
       },
       animation: {
         "caret-blink": "caret-blink 1.25s ease-out infinite",
@@ -122,8 +106,6 @@ const config = {
         "accordion-up": "accordion-up 0.2s ease-out",
         scroll:
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
